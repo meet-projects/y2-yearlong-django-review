000dc88e0ce2c060bbcc3f9830cc11fd2ee0b0fe
mprat <michele.pratusevich@gmail.com>
2013-06-25 12:07:53 +0300

    actually make it multiply instead of add

--- learn/day1/views.py
+++ learn/day1/views.py
@@ -4,4 +4,4 @@ def some_text(request):
 	return HttpResponse("This is some text response!")
 
 def multiply_numbers(request, num1, num2):
-	return HttpResponse(str(int(num1) + int(num2)))
+	return HttpResponse(str(int(num1) * int(num2)))
