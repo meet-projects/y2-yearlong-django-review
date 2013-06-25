f4a96d0ce418d79ca6c02ef271a83f2cf54e24c0
user <user@ubuntu.(none)>
2013-06-25 12:44:17 +0300

    views and settings changes

--- learn/day1/views.py
+++ learn/day1/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def some_text(request):
+	return render(request, 'newsfeed.html', {})
 	return HttpResponse("This is some text response!")
 
 def multiply_numbers(request, num1, num2):
