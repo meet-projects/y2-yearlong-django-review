aeb2b4ca3e6dc8e652ea54b0d9eab98a997a2b0b
mprat <michele.pratusevich@gmail.com>
2013-09-26 00:54:08 +0300

    dummy functions for 4 views

--- toplists/movies/models.py
+++ toplists/movies/models.py
@@ -6,6 +6,15 @@ class Movie(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        return self.title
+
+    def is_terrible(self):
+        return dislikes > likes
+
 class Comment(models.Model):
     text = models.CharField(max_length=1000)
     movie = models.ForeignKey(Movie)
+
+    def __unicode__(self):
+        return self.movie.title + ": " + self.text
--- toplists/movies/views.py
+++ toplists/movies/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.http import HttpResponse
+
+def movie_list(request):
+    return HttpResponse("this is the list of all movies")
+
+def movie(request):
+    return HttpResponse("this is where you see one movie and all it's comments")
+
+def add_movie(request):
+    return HttpResponse("adding movie")
+
+def add_comment(request):
+    return HttpResponse("adding comment")
