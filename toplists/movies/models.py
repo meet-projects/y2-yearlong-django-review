871ea96f13fbf5a6fe30cc6646e227c4c5ffa7d4
mprat <michele.pratusevich@gmail.com>
2013-09-26 02:56:11 +0300

    base templates added

--- toplists/movies/models.py
+++ toplists/movies/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=500)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
-
-    def is_terrible(self):
-        return dislikes > likes
 
 class Comment(models.Model):
     text = models.CharField(max_length=1000)
--- toplists/toplists/urls.py
+++ toplists/toplists/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 from movies import views
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('', 
     url(r'^movies$', views.movie_list),
