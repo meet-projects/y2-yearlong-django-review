06ea99da10ca44210c1cebb5b6c215f13b9a91fa
mprat <michele.pratusevich@gmail.com>
2013-09-25 22:58:33 +0300

    restarting

--- learn/day1/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def some_text(request):
-	return HttpResponse("This is some text response!")
-
-def multiply_numbers(request, num1, num2):
-	return HttpResponse(str(int(num1) * int(num2)))
--- learn/learn/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^day1/', include('day1.urls')),
-)
