fdbb84ebf8aa26431da79a688ff1fde7b3f1b720
mprat <michele.pratusevich@gmail.com>
2013-10-17 15:54:25 +0300

    one more fix for settings.py

--- toplists/toplists/settings.py
+++ toplists/toplists/settings.py
@@ -119,7 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(SITE_ROOT, 'movies', 'templates')
+    join(SITE_ROOT, appname, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -133,7 +133,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'movies'
+    appname
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
